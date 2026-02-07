c0d4c7be83f1c958b7b502ce5b145507dea8129b
Anil Thakur <anilt0234@gmail.com>
2026-02-07 22:23:42 +0530

    currying file modified

--- js/currying.js
+++ js/currying.js
@@ -1,7 +1,19 @@
-// function autoMailer(to)(
-//     return function(sub) {
-//         return function(body) {
-//             console.log(`mail has sent to ${to} with subject ${sub} and body ${body}`)
-//         }
-//     }
-// )
+// function autoMailer(to, sub, body){
+//     console.log(`mail has been sent to ${to} with subject ${sub} and body ${body}`);
+// }
+// autoMailer("anilt0234@gmail.com", "Order details", "Please find the attached order details.");
+
+
+
+
+
+
+function autoMailer(to){
+    return function(sub) {
+        return function(body) {
+            console.log(`mail has sent to ${to} with subject ${sub} and body ${body}`);
+        }
+    }
+}
+
+autoMailer("anilt0234@gmail.com")("Order details")("order placed.");
