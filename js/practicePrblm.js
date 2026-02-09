2102aea61a4a9707a38b7d419fbb13538ddfcf73
Anil Thakur <anilt0234@gmail.com>
2026-02-09 22:26:16 +0530

    Modiefied PracticePrblm

--- js/practicePrblm.js
+++ js/practicePrblm.js
@@ -39,23 +39,57 @@
 // Write a program to reverse the digits of a number using a loop.
 
 // let num = 12345;
+// let str = num.toString();
 // let reversed = "";
-//  for ( let i = num.toString.length - 1;  i>=0; i--){
-//     reversed+= num.toString.length - 1(i);
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//   reversed += str[i];
+// }
+
+// console.log(reversed)
+     
+// }
+// let res = sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+// console.log(res)
+
+
+// 5. Print Even Numbers
+// Print all even numbers from 1 to 50 using a loop.
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 6. Count the number of digits in a given number using a loop.
+
+// let num = 123456789;
+// let count = num.toString().length;
+// console.log(count);
+
+
+// 7. Fibonacci Series
+// Generate the first n terms of the Fibonacci sequence using a loop.
+
+// let n = 10;
+// let a = 0;
+// let b = 1;
+//  for ( let i = 1; i<= n; i++){
+//     console.log(a);
+//     next = a + b;
+//     a = b;
+//     b = next;
 //  }
-//  console.log(reversed)
 
 
+ 
+// 8. Sum of Digits
+// Find the sum of the digits of a number using a loop
 
-
-// function sum (a, b, c, d, ...args){
+// function sum ( ...args){
 //      let sum = 0;
 //       for (let i = 0; i < args.length; i++){
 //         sum+= args[i]
 //       }
 //       return sum;
-
-     
-// }
-// let res = sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-// console.log(res)
